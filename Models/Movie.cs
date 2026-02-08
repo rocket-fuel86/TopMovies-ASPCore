5300dcf4fcf1e170f1ec7b7524e8b113578100fa
rocket-fuel86 <NasaSpace65@gmail.com>
2026-02-08 12:34:59 +0200

    Added movie trailers

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,5 +9,6 @@
         public int Year { get; set; }
         public string? Poster { get; set; }
         public string? Description { get; set; }
+        public string? Trailer { get; set; }
     }
 }
